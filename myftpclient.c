8f83324f77195524fe8b28ae2beb11ee5b8c16f1
Memory_of_Star <1900012725@pku.edu.cn>
2021-09-30 18:08:12 -0700

    UDP echo implemented

--- myftpclient.c
+++ myftpclient.c
@@ -1,2 +1,25 @@
 #include "myftp.h"
 
+int main(int argc, char **argv)
+{
+    int sockfd, n;
+    struct sockaddr_in servaddr;
+    char sendline[MAXLINE], recvline[MAXLINE + 1];
+    if (argc != 2){
+        fprintf(stderr, "%s\n", "usage: echoCli <IPaddress>");
+        exit(1);
+    }
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+    servaddr.sin_port = htons(4000);
+    
+    while (fgets(sendline, MAXLINE, stdin) != NULL){
+        sendto(sockfd, sendline, strlen(sendline), 0, (struct sockaddr *)&servaddr, sizeof(servaddr));
+        n = recvfrom(sockfd, recvline, MAXLINE, 0, NULL, NULL);
+        recvline[n] = 0;
+        fputs(recvline, stdout);
+    }
+    exit(0);
+}
--- myftpserver.c
+++ myftpserver.c
@@ -1,2 +1,21 @@
 #include "myftp.h"
 
+int main(int argc, char **argv){
+    char mesg[MAXLINE];
+    int sockfd, n, len;
+    struct sockaddr_in cliaddr, servaddr;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(4000);
+
+    bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+    for(;;){
+        len = sizeof(cliaddr);
+        n = recvfrom(sockfd, mesg, MAXLINE, 0, (struct sockaddr *)&cliaddr, &len);
+        sendto(sockfd, mesg, n, 0, (struct sockaddr *)&cliaddr, len);
+    }
+}
